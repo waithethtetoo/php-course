13a81a2c4c54aff6d0289319fea154a6c91fcefa
waithethtetoo <4itcourse@gmail.com>
2020-10-03 13:46:01 +0630

    conclusion

--- index.php
+++ index.php
@@ -15,27 +15,34 @@
 			$products['Car'] = 15000;
 			$products['iPhone'] = 1000;
 			$products['Toaster'] = 75;
-			foreach ($products as $key => $value) {
-				echo "<p>The ".$key." costs ".$value."</p>";
-			}
 			
-			echo "<h2>Items you can afford</h2>";
-			foreach ($products as $key => $value) {
-				if ($value <= $credit) {
-					echo "<p>".$key."</p>";
-				}
-			}
-
-			$amount = 800;
 			$taxRate = 0.0825;
-			$addedTax = $amount * $taxRate;
-			echo $addedTax;
 
 			function tax_calc($amount,$tax){
 				$calculate_tax = $amount * $tax;
 				$amount = round($amount + $calculate_tax, 2);
 				return $amount;
 			}
+
+			foreach ($products as $key => $value) {
+				$costWithTax = tax_calc($value,$taxRate);
+				echo "<p>The ".$key." costs ".$costWithTax." with tax</p>";
+			}
+
+			echo "<h2>Items you can afford</h2>";
+			foreach ($products as $key => $value) {
+				$costWithTax = tax_calc($value,$taxRate);
+				if ($costWithTax <= $credit) {
+					echo "<p>".$key."</p>";
+				}
+			}
+
+			$amount = 800;
+			
+			$addedTax = $amount * $taxRate;
+			echo $addedTax;
+
+			
 			echo tax_calc(750,0.223);
 		?>
 	</body>
