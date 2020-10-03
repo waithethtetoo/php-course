867159cbed71030b60d9e258daec25f2695d3f2b
waithethtetoo <4itcourse@gmail.com>
2020-10-03 13:21:01 +0630

    add variables

--- index.php
+++ index.php
@@ -5,7 +5,10 @@
 	</head>
 	<body>
 		<?php
-			echo "<h1> Welcome!</h1>";
+			$name = "PHP Store";
+			$credit = 1000;
+			echo "<h1> Welcome to ".$name."!</h1>";
+			echo "<h2>You have $".$credit." in your wallet.</h2>";
 		?>
 	</body>
 </html>
